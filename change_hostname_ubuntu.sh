ab7b9d07e090997e142b6e61a0e268ac77a2b2d0
ericchenkai <ericchenkai@gmail.com>
2017-07-17 17:15:24 +0800

    Update change_hostname_ubuntu.sh

--- change_hostname_ubuntu.sh
+++ change_hostname_ubuntu.sh
@@ -8,11 +8,15 @@ else
 fi
 
 sudo hostname $NEW_HOSTNAME
+echo "1"
 sudo sed -i "s/127.0.1.1.*/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+echo "2"
 sudo sed -i "s/.*/$NEW_HOSTNAME/g" /etc/hostname
-exec bash
+echo "3"
 
 echo "The hostname is now applied as $NEW_HOSTNAME"
 echo "###### NOTE ######"
 echo "FYI: your previous hostname WAS $OLD_HOSTNAME"
 echo "You may run this script again to change it back"
+
+exec bash
